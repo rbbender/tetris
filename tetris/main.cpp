15c6b915bf6195bcd5e42bfdb80dddeada9e8940
Vasilii Gratcilevskii <vgratsil@gmail.com>
2019-04-14 18:07:23 -0700

    Some basic figure-field links

---
+++ tetris/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class CTFigurePos {
+	int size_x;
+	int size_y;
+	int** layout;
+public:
+	CTFigurePos(int sz_x, int sz_y, int** arr) : size_x(sz_x), size_y(sz_y), layout(arr) {};
+};
+
+
+
+int main() {
+
+}
