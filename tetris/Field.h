90bc2ebca3f82f6d5abb67e716b772b6299d4e32
Vasilii Gratcilevskii <vgratsil@gmail.com>
2019-04-16 22:44:29 -0700

    Moved methods

--- tetris/Field.h
+++ tetris/Field.h
@@ -21,16 +21,16 @@ class Field {
 	typedef std::vector<Row>::iterator y_itr;
 	bool is_position_possible(const STFigurePos& ps, int p_x, int p_y);
 	bool test_landed(int p_x, int p_y);
-public:
-	Field(int size_x = 10, int size_y = 20);
-	void dbg_print();
-	int set_current_figure(CFigure* fig, int pos_x, int pos_y);
-	int remove_current_figure();
-	int merge_current_figure();
 	bool can_move_right();
 	bool can_move_left();
 	bool can_rotate_cw();
 	bool can_rotate_ccw();
+	int remove_current_figure();
+	int merge_current_figure();
+public:
+	Field(int size_x = 10, int size_y = 20);
+	void dbg_print();
+	int set_current_figure(CFigure* fig, int pos_x, int pos_y);
 	bool is_landed();
 	bool is_game_over();
 	int force_land();
